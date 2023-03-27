1c718ee0f5fd47c570c990e8c0aa6b7e80406451
Sanderhalvors1 <sanderhalvorsen@icloud.com>
2023-03-27 14:39:50 +0200

    nye endringer

--- yr/yr_test.go
+++ yr/yr_test.go
@@ -1,1 +1,26 @@
 package yr
+
+import (
+	"os"
+	"testing"
+)
+
+func TestCheckCSVLineCount(t *testing.T) {
+	// Åpner CSV-filen for testing
+	file, err := os.Open("kjevik-temp-celsius-20220318-20230318.csv")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer file.Close()
+
+	// Leser innholdet i CSV-filen
+	lines, err := lesLinjer(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Sjekker om antall linjer er lik 16756
+	if len(lines) != 16755 {
+		t.Errorf("Forventet 16756 linjer, fikk %d", len(lines))
+	}
+}
